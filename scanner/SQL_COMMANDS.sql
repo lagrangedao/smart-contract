36f52d9e77f8253ef893513faef72959b2cb3441
Charles Cao <caotaotao@gmail.com>
2023-02-10 11:16:25 -0500

    update the db schema and creation.

--- scanner/SQL_COMMANDS.sql
+++
@@ -1,3 +0,0 @@
-create database HYPERSPACE;
-
-create table HYPERSPACE.depositTransactions( blocknumber INT PRIMARY KEY, event VARCHAR(10) NOT NULL, accountAddress VARCHAR(100) NOT NULL, recepientAddress VARCHAR(100) NOT NULL, amount BIGINT, TxHash VARCHAR(1000), TxTimeStamp BIGINT);
