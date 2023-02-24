7bf5f91aac447bc33d4e88d81b67f329bcb3cdf8
rykci <yuen.ricky@hotmail.com>
2023-02-24 18:18:14 -0500

    check user balance before calling contract

--- scripts/buy-space.py
+++ scripts/buy-space.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 print('''
-                                           _  _           
+
 888       888          888                                              888                 8888888888 8888888888 888     888 888b     d888 
 888   o   888          888                                              888                 888        888        888     888 8888b   d8888 
 888  d8b  888          888                                              888                 888        888        888     888 88888b.d88888 
@@ -25,8 +25,7 @@ print('''
 88888P Y88888 88888888 888 888     888  888 888  888  888 88888888      888   888  888      888        888          Y88o88P   888  Y8P  888 
 8888P   Y8888 Y8b.     888 Y88b.   Y88..88P 888  888  888 Y8b.          Y88b. Y88..88P      888        888           Y888P    888   "   888 
 888P     Y888  "Y8888  888  "Y8888P "Y88P"  888  888  888  "Y8888        "Y888 "Y88P"       888        8888888888     Y8P     888       888 
-                                                                                                                                            
-                                                                                                                                                                                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                                                                                                                                                         
 ''')
 
 rpc_endpoint = os.getenv("rpc_endpoint")
@@ -64,11 +63,15 @@ space_contract = web3.eth.contract(address=space_contract_address, abi=space_abi
 token_contract = web3.eth.contract(address=token_contract_address, abi=token_abi)
 token_name, token_symbol = token_contract.functions.name().call(), token_contract.functions.symbol().call()
 token_decimals = token_contract.functions.decimals().call()
-
 token_balance = token_contract.functions.balanceOf(wallet_address).call()
+fil_balance = web3.eth.getBalance(wallet_address)
+
+user_balance = space_contract.functions.balanceOf(wallet_address).call()
+lad_allowance = token_contract.functions.allowance(wallet_address, space_contract_address).call()
 
 print(f"Your payment wallet address: {wallet_address}")
-print(f"Your Account balance: {token_balance / (10 ** token_decimals):,} {token_symbol}")
+print(f"Your Account balance: {fil_balance / (10 ** token_decimals):f} tFIL")
+print(f"Your Account balance: {token_balance / (10 ** token_decimals):f} {token_symbol}\n")
 # print(f"epoch time: 1 block per {epoch_time} seconds\n")
 
 hardware_type = input(" 0. CPU Only - 2 vCPU - 16 GiB - Free \
@@ -86,6 +89,7 @@ blocks = input("How many blocks: ")
 
 prices = [0, 1, 20, 30, 35, 105]
 price = prices[int(hardware_type)] * int(blocks)
+price_in_wei = int(price) * 10 ** token_decimals
 
 # Fat-fingering check
 print(f"Confirm purchasing hardware type {hardware_type} for {blocks} blocks ({price} {token_symbol})?")
@@ -94,29 +98,35 @@ if not confirm.lower().startswith("y"):
     print("Aborted")
     sys.exit(1)
 
-print(f"\nDepositing {price} {token_symbol} into contract...")
-nonce = web3.eth.getTransactionCount(wallet_address)
-tx_config["nonce"] = nonce
-approve_tx = token_contract.functions.approve(space_contract_address,
-                                              int(price) * 10 ** token_decimals).buildTransaction(tx_config)
+if token_balance < price_in_wei:
+    print("\nNot enough LAD tokens")
+    sys.exit(1)
 
-# SIGN TX
-signed_tx = web3.eth.account.signTransaction(approve_tx, private_key)
-tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-web3.eth.wait_for_transaction_receipt(tx_hash)
-hash = web3.toHex(tx_hash)
+if user_balance < price_in_wei and int(hardware_type) != 0: # user needs to deposit LAD into contract
+    if lad_allowance < price_in_wei: # user needs to approve spending LAD
+        nonce = web3.eth.getTransactionCount(wallet_address)
+        tx_config["nonce"] = nonce
+        approve_tx = token_contract.functions.approve(space_contract_address,
+                                                    price_in_wei).buildTransaction(tx_config)
 
-nonce = web3.eth.getTransactionCount(wallet_address)
-tx_config["nonce"] = nonce
-deposit_tx = space_contract.functions.deposit(int(price) * 10 ** token_decimals).buildTransaction(tx_config)
+        # SIGN TX
+        signed_tx = web3.eth.account.signTransaction(approve_tx, private_key)
+        tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        web3.eth.wait_for_transaction_receipt(tx_hash)
+        hash = web3.toHex(tx_hash)
 
-# SIGN TX
-signed_tx = web3.eth.account.signTransaction(deposit_tx, private_key)
-tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-web3.eth.wait_for_transaction_receipt(tx_hash)
-hash = web3.toHex(tx_hash)
+    print(f"\nDepositing {price} {token_symbol} into contract...")
+    nonce = web3.eth.getTransactionCount(wallet_address)
+    tx_config["nonce"] = nonce
+    deposit_tx = space_contract.functions.deposit(price_in_wei).buildTransaction(tx_config)
 
-print(f"Deposit is completed. Tx Hash: https://hyperspace.filfox.info/en/message/{hash}")
+    # SIGN TX
+    signed_tx = web3.eth.account.signTransaction(deposit_tx, private_key)
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    web3.eth.wait_for_transaction_receipt(tx_hash)
+    hash = web3.toHex(tx_hash)
+
+    print(f"Deposit is completed. Tx Hash: https://hyperspace.filfox.info/en/message/{hash}")
 
 print(f"\npurchasing space...")
 nonce = web3.eth.getTransactionCount(wallet_address)
