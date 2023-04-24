f96faa79f4d972faab81cf23008972eaf53deed0
Rishabh42 <rishabhthaney@gmail.com>
2023-04-24 14:02:51 -0400

    refactored nftScanner, using classes and objects

--- scanner/nftScanner.py
+++ scanner/nftScanner.py
@@ -10,172 +10,119 @@ import warnings
 
 polygon_url = "https://polygon-mumbai.g.alchemy.com/v2/JpRokS66sMaDD680W2NWwqhLuqDC1f7l"
 
-# MySQL DB:
-try:
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user=config('DB_USER'),
-        password=config('DB_PASSWORD'),
-        database='nft_data'
-    )
-    mycursor = mydb.cursor()
-    print("MySQL connection successful")
-except mysql.connector.Error as err:
-    print(f"An error occurred while connecting to MySQL: {err}")
+class NFTScanner:
+    def __init__(self, cf_contract_address, so_contract_address, from_block):
+        self.cf_contract_address = cf_contract_address
+        self.so_contract_address = so_contract_address
+        self.cf_abi_file_path = '../contracts/abi/LagrangeChainlinkData.json'
+        self.so_abi_file_path = '../contracts/abi/LagrangeChainlinkDataConsumer.json'
+        self.from_block = from_block
+        self.batch_size = 1000
+        self.mydb = mysql.connector.connect(
+            host="localhost",
+            user=config('DB_USER'),
+            password=config('DB_PASSWORD'),
+            database='nft_data'
+        )
+        self.w3 = Web3(Web3.HTTPProvider(polygon_url))
+        self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+        self.cf_abi = json.load(open(self.cf_abi_file_path))
+        self.so_abi = json.load(open(self.so_abi_file_path))
+        self.mycursor = self.mydb.cursor()
+        self.update_owner_command = 'UPDATE nft_ownership SET transfer_event_block = (%s), owner_address = (%s) WHERE nft_address = (%s) AND nft_ID=(%s)'
+        self.is_nft_exists_command = 'SELECT * from nft_ownership WHERE nft_address = (%s) AND nft_ID=(%s)'
 
-# HTTPProvider:
-w3 = Web3(Web3.HTTPProvider(polygon_url))
-w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    def start_NFT_scan(self, target_block):
+        while self.from_block < target_block:
+            warnings.filterwarnings("ignore")
 
-# res = w3.isConnected()
-# print("res: ",res)
+            to_block = self.from_block + self.batch_size
+            print(self.from_block,to_block)
 
-# Chainlink Functions Data NFT (mumbai):
-CF_CONTRACT_ADDRESS = '0xD81288579c13e26F621840B66aE16af1460ebB5a'
-cf_AddrDict=[]
-cf_AddrDict.append(CF_CONTRACT_ADDRESS)
+            cf_contract = self.w3.eth.contract(address=Web3.toChecksumAddress(self.cf_contract_address), abi=self.cf_abi)
+            so_contract = self.w3.eth.contract(address=Web3.toChecksumAddress(self.so_contract_address), abi=self.so_abi)
 
-# Chainlink Single-Oracle Data NFT (mumbai):
-SO_CONTRACT_ADDRESS = '0x923AfAdE5d2c600b8650334af60D6403642c1bce'
-so_AddrDict=[]
-so_AddrDict.append(SO_CONTRACT_ADDRESS)
+            cf_transfer_events = cf_contract.events.Transfer.getLogs(fromBlock=self.from_block, toBlock=to_block)
+            so_transfer_events = so_contract.events.Transfer.getLogs(fromBlock=self.from_block, toBlock=to_block)
 
-cf_abi_file = open('../contracts/abi/LagrangeChainlinkData.json')
-so_abi_file = open('../contracts/abi/LagrangeChainlinkDataConsumer.json')
-CF_ABI = json.load(cf_abi_file)
-SO_ABI = json.load(so_abi_file)
+            if cf_transfer_events:
+                cf_event_size = len(cf_transfer_events)
+                i = 0
 
-# validate NFT contract address
-# is_address_valid = w3.isAddress(CONTRACT_ADDRESS)
-# print("isAddrValid: ",is_address_valid)
+                while i < cf_event_size:
+                    if cf_transfer_events[i].args["from"] != '0x0000000000000000000000000000000000000000':
 
-# Block on which the contract was deployed:
-from_block = 34492518
-target_block = w3.eth.get_block('latest') #34294743
-# Block chunk to be scanned:
-batchSize = 1000
+                        token_id = cf_transfer_events[i].args.tokenId
 
-updateOwnerCommand='UPDATE nft_ownership SET transfer_event_block = (%s), owner_address = (%s) WHERE nft_address = (%s) AND nft_ID=(%s)'
-isNFTexistsCommand='SELECT * from nft_ownership WHERE nft_address = (%s) AND nft_ID=(%s)'
+                        nft_check_params = (self.cf_contract_address, token_id)
 
-while from_block < target_block.number:
-    # silence harmless warnings
-    warnings.filterwarnings("ignore")
+                        self.mycursor.execute(self.is_nft_exists_command, nft_check_params)
+                        nft_exists_check = self.mycursor.fetchall()
 
-    toBlock = from_block + batchSize
-    print(from_block,toBlock)
+                        if nft_exists_check:
+                            cf_update_params = [
+                                cf_transfer_events[i].blockNumber,
+                                cf_transfer_events[i].args.to,
+                                self.cf_contract_address,
+                                token_id
+                            ]
 
-    cf_contract = w3.eth.contract(address=Web3.toChecksumAddress(CF_CONTRACT_ADDRESS), abi=CF_ABI)
-    so_contract = w3.eth.contract(address=Web3.toChecksumAddress(SO_CONTRACT_ADDRESS), abi=SO_ABI)
+                            try:
+                                self.mycursor.execute(self.update_owner_command, cf_update_params)
+                                self.mydb.commit()
+                                print(f"Updated owner for NFT Address: {self.cf_contract_address}")
+                            except e:
+                                print(f"An error occurred while updating owner for NFT Address {CF_CONTRACT_ADDRESS}: {e}")
+                        else:
+                            print(f"Following NFT address does not exist in the DB: {CF_CONTRACT_ADDRESS}")
 
-    cfTransferEvents = cf_contract.events.Transfer.getLogs(fromBlock=from_block, toBlock=toBlock)
-    soTransferEvents = so_contract.events.Transfer.getLogs(fromBlock=from_block, toBlock=toBlock)
-    # print("cfTransferEvents: ",cfTransferEvents)
-    # print("--------------------")
-    # print("soTransferEvents: ",soTransferEvents)
+                    i=i+1
 
-    # eventOwnership = ContractEvent('OwnershipTransferred', abi=ABI)
-    # eventLog=event.processLog(log)
-    # print("event: ",eventLog)
+            if so_transfer_events:
+                so_event_size = len(so_transfer_events)
+                i = 0
 
-    if cfTransferEvents:
-        cfEventSize=len(cfTransferEvents)
-        # print("cfEventSize: ", cfEventSize)
-        i=0
+                while i < so_event_size:
+                    if so_transfer_events[i].args["from"] != '0x0000000000000000000000000000000000000000':
 
-        while i<cfEventSize:
-            if cfTransferEvents[i].args["from"] != '0x0000000000000000000000000000000000000000':
-                # prevOwner = cfTransferEvents[i].args["from"]
+                        token_id = so_transfer_events[i].args.tokenId
 
-                tokenID = cfTransferEvents[i].args.tokenId
+                        nft_check_params = (self.so_contract_address, token_id)
 
-                nftCheckParams=(CF_CONTRACT_ADDRESS,tokenID)
-                # nftCheckParams=[]
-                # nftCheckParams.append(CF_CONTRACT_ADDRESS)
-                # nftCheckParams.append(tokenID)
-                mycursor.execute(isNFTexistsCommand,nftCheckParams)
-                nftExistsCheck = mycursor.fetchall()
-                # print("nftExistsCheck: ", nftExistsCheck!=False)
+                        self.mycursor.execute(self.is_nft_exists_command, nft_check_params)
+                        nft_exists_check = self.mycursor.fetchall()
 
-                if nftExistsCheck!=False :
+                        if nft_exists_check:
+                            so_update_params = [
+                                so_transfer_events[i].blockNumber,
+                                so_transfer_events[i].args.to,
+                                self.so_contract_address,
+                                token_id
+                            ]
 
-                    cfUpdateParams=[]
-                    cfUpdateParams.append(cfTransferEvents[i].blockNumber)
-                    cfUpdateParams.append(cfTransferEvents[i].args.to)
-                    cfUpdateParams.append(CF_CONTRACT_ADDRESS)
-                    cfUpdateParams.append(tokenID)
+                            try:
+                                self.mycursor.execute(self.update_owner_command, so_update_params)
+                                self.mydb.commit()
+                                print(f"Updated owner for NFT Address: {self.so_contract_address}")
+                            except e:
+                                print(f"An error occurred while updating owner for NFT Address {SO_CONTRACT_ADDRESS}: {e}")
+                        else:
+                            print(f"Following NFT address does not exist in the DB: {SO_CONTRACT_ADDRESS}")
 
-                    # print("cfUpdateParams: ",cfUpdateParams)
-                    try:
-                        # Update the NFT owner in the DB 
-                        mycursor.execute(updateOwnerCommand,cfUpdateParams)
-                        mydb.commit()
-                        print(f"Updated owner for NFT Address: {CF_CONTRACT_ADDRESS}")
-                    except e:
-                        print(f"An error occurred while updating owner for NFT Address {CF_CONTRACT_ADDRESS}: {e}")
+                    i=i+1
 
-                     # Get the previous owner's address
-                     # ownerAddressCommand="select owner_address from nft_ownership WHERE nft_address = (%s) "
-                     # mycursor.execute(ownerAddressCommand,cf_AddrDict)
-                     # ownerAddr = mycursor.fetchall()
-                     # print("NFT owner: ",ownerAddr[0][0])
+            self.from_block = self.from_block + self.batch_size + 1
+            blockDiff = target_block - self.from_block
 
-                     # print(cfTransferEvents[i].blockNumber)
-                     # print(cfTransferEvents[i].event)
-                     # print(cfTransferEvents[i].args["from"])
-                     # print(cfTransferEvents[i].args.to)
-                     # print(cfTransferEvents[i].args.tokenId)
-                     # print(cfTransferEvents[i].address)
-                else:
-                    print(f"Following NFT address does not exist in the DB:{CF_CONTRACT_ADDRESS}")
-            i=i+1
+            # print("target_block: ",target_block.number)
+            # print("batchSize: ",batchSize)
+            # print("from_block ",from_block)
+            # print("blockDiff: ",blockDiff)
 
-    if soTransferEvents:
-        soEventSize=len(soTransferEvents)
-        # print("soEventSize: ",soEventSize)
-        i=0
+            if(blockDiff < self.batch_size):
+                batchSize = blockDiff
 
-        while i<soEventSize:
-            if soTransferEvents[i].args["from"] != '0x0000000000000000000000000000000000000000':
-                # prevOwner = cfTransferEvents[i].args["from"]
 
-                tokenID = soTransferEvents[i].args.tokenId
-
-                soNFTCheckParams=(SO_CONTRACT_ADDRESS,tokenID)
-                # soNFTCheckParams.append(SO_CONTRACT_ADDRESS)
-                # soNFTCheckParams.append(tokenID)
-                mycursor.execute(isNFTexistsCommand,soNFTCheckParams)
-                soNFTExistsCheck = mycursor.fetchall()
-
-                if soNFTExistsCheck!=False:
-
-                    soUpdateParams=[]
-                    soUpdateParams.append(soTransferEvents[i].blockNumber)
-                    soUpdateParams.append(soTransferEvents[i].args.to)
-                    soUpdateParams.append(SO_CONTRACT_ADDRESS)
-                    soUpdateParams.append(tokenID)
-
-                    # print("soUpdateParams: ",soUpdateParams)
-                    try:
-                        # Update the NFT owner in the DB
-                        mycursor.execute(updateOwnerCommand,soUpdateParams)
-                        mydb.commit()
-                        print(f"Updated owner for NFT Address: {SO_CONTRACT_ADDRESS}")
-                    except e:
-                        print(f"An error occurred while updating owner for NFT Address {SO_CONTRACT_ADDRESS}: {e}")
-
-                else:
-                    print(f"Following NFT address does not exist in the DB: {SO_CONTRACT_ADDRESS}")
-
-            i=i+1
-
-    from_block = from_block + batchSize + 1
-    blockDiff = target_block.number - from_block
-
-    # print("target_block: ",target_block.number)
-    # print("batchSize: ",batchSize)
-    # print("from_block ",from_block)
-    # print("blockDiff: ",blockDiff)
-
-    if(blockDiff < batchSize):
-        batchSize = blockDiff
+scanner_0bj = NFTScanner('0xD81288579c13e26F621840B66aE16af1460ebB5a','0x923AfAdE5d2c600b8650334af60D6403642c1bce',34492518)
+target_block = scanner_0bj.w3.eth.get_block('latest')
+scanner_0bj.start_NFT_scan(target_block.number)
