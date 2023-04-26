c382e5412ea5f6411bdcddeb31164b8e52784e9b
Rishabh42 <rishabhthaney@gmail.com>
2023-04-25 23:48:38 -0400

    logger config

--- scanner/nftScanner.py
+++ scanner/nftScanner.py
@@ -9,6 +9,17 @@ from hexbytes import HexBytes
 import warnings
 import logging
 
+# set up logging to file
+# logging.basicConfig(level=logging.INFO,
+#                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+#                     datefmt='%m-%d %H:%M',
+#                     filename='NFTscan.log')
+# # define a Handler which writes INFO messages or higher to the sys.stderr
+# console = logging.StreamHandler()
+# console.setLevel(logging.INFO)
+# # add the handler to the root logger
+# logging.getLogger('').addHandler(console)
+
 polygon_url = config('POLYGON_URL')
 
 class NFTScanner:
