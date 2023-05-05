fed9b24794b310700a0376c823ec90cb1f971bbc
Rishabh42 <rishabhthaney@gmail.com>
2023-05-05 01:58:05 -0400

    using sql orm for get last scanned block

--- scanner/nftScanner.py
+++ scanner/nftScanner.py
@@ -2,6 +2,9 @@ from web3 import Web3
 from decouple import config
 from web3.middleware import geth_poa_middleware
 from web3.contract import ContractEvent
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 import time
 import mysql.connector
 import json
@@ -21,6 +24,20 @@ logging.getLogger('').addHandler(console)
 
 polygon_url = config('POLYGON_URL')
 
+Base = declarative_base()
+
+class NFTData(Base):
+    __tablename__ = 'nft_ownership'
+    id = Column(Integer, primary_key=True)
+    last_scan_block = Column(Integer)
+    transfer_event_block = Column(Integer)
+    owner_address = Column(String)
+    nft_address = Column(String)
+    nft_ID = Column(Integer)
+
+engine = create_engine('mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
+Session = sessionmaker(bind=engine)
+
 class NFTScanner:
     def __init__(self, cf_contract_address, so_contract_address):
         # Data NFT with Chainlink functions contract address
@@ -33,38 +50,76 @@ class NFTScanner:
         self.so_abi_file_path = '../contracts/abi/LagrangeChainlinkDataConsumer.json'
 
         # DB connection
-        self.mydb = mysql.connector.connect(
-            host="localhost",
-            user=config('DB_USER'),
-            password=config('DB_PASSWORD'),
-            database='nft_data'
-        )
+        # self.mydb = mysql.connector.connect(
+        #     host="localhost",
+        #     user=config('DB_USER'),
+        #     password=config('DB_PASSWORD'),
+        #     database='nft_data'
+        # )
+
+        # DB connection
+        self.engine = create_engine('mysql+mysqlconnector://' + config('DB_USER') + ':' + config('DB_PASSWORD') + '@localhost/nft_data')
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+
         self.w3 = Web3(Web3.HTTPProvider(polygon_url))
         self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         self.cf_abi = json.load(open(self.cf_abi_file_path))
         self.so_abi = json.load(open(self.so_abi_file_path))
-        self.mycursor = self.mydb.cursor()
+        # self.mycursor = self.mydb.cursor()
 
-        getLastScanBlockCommand= 'select last_scan_block from nft_ownership'
-        self.mycursor.execute(getLastScanBlockCommand)
-        lastScannedBlock = self.mycursor.fetchall()
+        # getLastScanBlockCommand= 'select last_scan_block from nft_ownership'
+        # self.mycursor.execute(getLastScanBlockCommand)
+        # lastScannedBlock = self.mycursor.fetchall()
+
+        # if lastScannedBlock:
+        #     self.from_block = lastScannedBlock[0][0] + 1
+        # else:
+        #     # Block at which the contracts were deployed
+        #     self.from_block = 34333512
+        lastScannedBlock = self.session.query(NFTData.last_scan_block).first()
+        print("lastScannedBlock: ",lastScannedBlock[0])
 
         if lastScannedBlock:
-            self.from_block = lastScannedBlock[0][0] + 1
+            self.from_block = lastScannedBlock[0] + 1
         else:
             # Block at which the contracts were deployed
             self.from_block = 34333512
 
         self.batch_size = 1000
 
+        self.session = Session()
         # print("lastScannedBlock: ",lastScannedBlock[0][0])
 
         # Update owner command
         self.update_owner_command = 'UPDATE nft_ownership SET transfer_event_block = (%s), owner_address = (%s) WHERE nft_address = (%s) AND nft_ID=(%s)'
+        # self.update_owner_command = self.session.query(NFT).filter_by(nft_address=self.cf_contract_address, nft_ID=token_id).first()
         # Is NFT exists check
         self.is_nft_exists_command = 'SELECT * from nft_ownership WHERE nft_address = (%s) AND nft_ID=(%s)'
         # Insert new NFT command:
         self.insert_NFT_command = 'INSERT INTO nft_ownership (last_scan_block,transfer_event_block,owner_address,nft_address,nft_ID) VALUES (%s,%s,%s,%s,%s)'
+
+    def update_ow ner_command(self, blockNumber, to, cf_contract_address, token_id):
+        try:
+            nft_ownership = self.session.query(NFTOwnership).filter_by(nft_address=cf_contract_address, nft_ID=token_id).first()
+            if nft_ownership:
+                nft_ownership.transfer_event_block = blockNumber
+                nft_ownership.owner_address = to
+            else:
+                nft_ownership = NFTOwnership(
+                    last_scan_block=self.from_block,
+                    transfer_event_block=blockNumber,
+                    owner_address=to,
+                    nft_address=cf_contract_address,
+                    nft_ID=token_id
+                )
+                self.session.add(nft_ownership)
+            self.session.commit()
+            return True
+        except SQLAlchemyError as e:
+            print("Error updating NFT ownership: ", e)
+            self.session.rollback()
+            return False
 
     def start_NFT_scan(self, target_block):
         while self.from_block < target_block:
