350d9fbcd5060770fb20db6444fff4b2c2c49e29
Rishabh42 <rishabhthaney@gmail.com>
2023-05-16 14:39:55 -0400

    DataNFTFactory ABI and updated the factory mint script to work with Sepolia

--- scripts/factory-mint.py
+++ scripts/factory-mint.py
@@ -21,7 +21,7 @@ web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 block_number = web3.eth.block_number
 print(f"Connected to {rpc_endpoint}, chain id is {web3.eth.chain_id}. the latest block is {block_number:,}\n")
 
-factory_contract_address = '0xCE35818cA65cfFEbbCF3a3444d57a93ba110Bd04'
+factory_contract_address = '0x26FE0adD600965518c06c25CCff182115917C34A'
 factory_contract_abi_file = open('../contracts/abi/DataNFTFactory.json')
 factory_contract_abi = json.load(factory_contract_abi_file)
 
@@ -37,15 +37,15 @@ assert web3.isChecksumAddress(factory_contract_address), f"Not a valid contract 
 factory_contract = web3.eth.contract(address=factory_contract_address, abi=factory_contract_abi)
 
 # call requestDataNFT function
-nft_uri = 'https://2d9999d121.calibration-swan-acl.filswan.com/ipfs/QmZEPZos8pExSSqfZwi4RKrLHUGBgQ5KsHMP3poyPMBomA'
+nft_uri = 'https://3b46ed854.acl.multichain.storage/ipfs/QmfWumvNSWTTXS6QTkmsEunDtUqGTwFyGNDp8bMCxxUK6y'
 # transaction = factory_contract.functions.requestDataNFT(nft_uri).transact()
 
 tx_config = {
     'chainId': web3.eth.chain_id,
     'from': wallet_address,
-    # 'gas': 410224,
-    # 'maxFeePerGas': web3.toWei('100', 'gwei'),
-    # 'maxPriorityFeePerGas': web3.toWei('25', 'gwei'),
+    # 'gas': 2000000,
+    'maxFeePerGas': web3.toWei('50', 'gwei'),
+    'maxPriorityFeePerGas': web3.toWei('10', 'gwei'),
 }
 nonce = web3.eth.get_transaction_count(wallet_address)
 tx_config["nonce"] = nonce
@@ -64,35 +64,47 @@ print("hash: ",hash)
 txInputReceipt = web3.eth.get_transaction_receipt(tx_hash)
 TxLogs = txInputReceipt.logs
 request_id = TxLogs[0].topics[1].hex()
+print("request_id: ",request_id)
 
 # Wait for the oracle to complete the request
 time.sleep(10)
 
 # Call the `requestData` mapping to check if the request was fulfilled
-request_data_tx = factory_contract.functions.requestData(request_id).call()
+# request_data_tx = factory_contract.functions.requestData(request_id).call()
+# print("request_data_tx: ",request_data_tx)
 
-# Store the result of `requestData` in the respective variables
-tx_owner = request_data_tx[0]
-tx_uri = request_data_tx[1]
-tx_deployable = request_data_tx[2]
-tx_fulfilled = request_data_tx[3]
+# # Store the result of `requestData` in the respective variables
+# tx_owner = request_data_tx[0]
+# tx_uri = request_data_tx[1]
+# tx_deployable = request_data_tx[2]
+# tx_fulfilled = request_data_tx[3]
 
-# Check if the correct owner is the one calling the method
-assert tx_owner == wallet_address
-# Check if `deployabe` is True
-assert tx_deployable == True
-# Check if `fulfilled` is True
-assert tx_fulfilled == True
+# # Check if the correct owner is the one calling the method
+# assert tx_owner == wallet_address
+# # Check if `deployabe` is True
+# assert tx_deployable == True
+# # Check if `fulfilled` is True
+# assert tx_fulfilled == True
+
+# print("------ Checks completed -------")
 
 nonce2 = web3.eth.get_transaction_count(wallet_address)
 tx_config["nonce"] = nonce2
 
 # Call the createDataNFT function
-nft_name = input(f"enter NFT name: ")
-nft_symbol = input(f"enter NFT symbol: ")
+# nft_name = input(f"enter NFT name: ")
+# nft_symbol = input(f"enter NFT symbol: ")
 
 # Build Tx with chainId, wallet address, nonce for creation of Data NFT from Factory
-create_dataNFT_tx = factory_contract.functions.createDataNFT(request_id,nft_name,nft_symbol).build_transaction(tx_config)
+# create_dataNFT_tx = factory_contract.functions.claimDataNFT(request_id).build_transaction(tx_config)
+
+# create_dataNFT_tx = factory_contract.functions.claimDataNFT(request_id).estimateGas(tx_config)
+create_dataNFT_tx = factory_contract.functions.claimDataNFT(request_id).buildTransaction({
+    'from': wallet_address,
+    'nonce': web3.eth.get_transaction_count(wallet_address),
+    'gas': 2000000,  # Adjust the gas limit as needed
+    'gasPrice': web3.toWei('5', 'gwei'),  # Adjust the gas price as needed
+})
 
 print(f"Deploying new NFT contract...")
 # SIGN TX for createDataNFT
@@ -100,8 +112,8 @@ createDataNFT_signed_tx = web3.eth.account.sign_transaction(create_dataNFT_tx, p
 createDataNFT_tx_hash = web3.eth.send_raw_transaction(createDataNFT_signed_tx.rawTransaction)
 createDataNFT_tx_receipt = web3.eth.wait_for_transaction_receipt(createDataNFT_tx_hash)
 createDataNFT_hash = web3.toHex(createDataNFT_tx_hash)
-# print("createDataNFT_hash: ",createDataNFT_hash)
+print("createDataNFT_hash: ",createDataNFT_hash)
 
-get_deployed_NFT_address = factory_contract.functions.uriToNftAddress(tx_uri).call()
-print(f"NFT contract successfully deployed. Contract address: {get_deployed_NFT_address}")
-print(f"View on Polygon scan: https://mumbai.polygonscan.com/tx/{createDataNFT_hash}")
+create_dataNFT_Txlogs = createDataNFT_tx_receipt['logs']
+print("create_dataNFT_Txlogs: ",create_dataNFT_Txlogs)
+
