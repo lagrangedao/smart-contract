8471575c182df154ff33dee8a22abca9a4d3146d
rykci <50771299+rykci@users.noreply.github.com>
2024-01-24 12:10:44 -0500

    fix deploy script

--- scripts/deployStaking.js
+++ scripts/deployStaking.js
@@ -4,19 +4,16 @@ const AR_WALLET = '0x47846473daE8fA6E5E51e03f12AbCf4F5eDf9Bf5'
 const AP_WALLET = '0x4BC1eE66695AD20771596290548eBE5Cfa1Be332'
 const ADMIN_WALLET = '0x29eD49c8E973696D07E7927f748F6E5Eacd5516D'
 
-const SWAN_CHAIN_USDC = '0x0c1a5A0Cd0Bb4A9F564f09Cc66f4c921B560371a'
-const SWAN_CHAIN_SWAN = '0x407a5856050053CF1DB54113bd9Ea9D2Eeee7C35'
+const SWAN_CHAIN_SWAN = '0x91B25A65b295F0405552A4bbB77879ab5e38166c'
 
-const SWAP = '0xaAc390a1A1C1BCF35261181207Ecf6f565dbacb5'
-
-const COLLATERAL = '0xA6848249CE6c591Af754A0780d352d2117F9F0b0'
+const COLLATERAL = ''
 const BIDDING = ''
 
 async function main() {
   const Collateral = await ethers.getContractFactory('CollateralContract')
   const Bidding = await ethers.getContractFactory('BiddingContract')
-  const USDC = await ethers.getContractFactory('USDC')
-  const usdc = USDC.attach(SWAN_CHAIN_USDC)
+  const Swan = await ethers.getContractFactory('Token')
+  let swan = Swan.attach(SWAN_CHAIN_SWAN)
   let collateral = ''
   let bidding = ''
 
@@ -25,7 +22,7 @@ async function main() {
     collateral = Collateral.attach(COLLATERAL)
   } else {
     console.log('Deploying Collateral Contract...')
-    collateral = await upgrades.deployProxy(Collateral, [SWAN_CHAIN_SWAN], {
+    collateral = await upgrades.deployProxy(Collateral, [], {
       initializer: 'initialize',
     })
     await collateral.waitForDeployment()
@@ -40,14 +37,7 @@ async function main() {
     console.log('Deploying Bidding Contract...')
     bidding = await upgrades.deployProxy(
       Bidding,
-      [
-        AR_WALLET,
-        AP_WALLET,
-        collateral.target,
-        //   SWAN_CHAIN_USDC,
-        SWAN_CHAIN_SWAN,
-        //   SWAP,
-      ],
+      [AR_WALLET, AP_WALLET, collateral.target, SWAN_CHAIN_SWAN],
       {
         initializer: 'initialize',
       },
@@ -69,7 +59,7 @@ async function main() {
 
   console.log('Approving spending for Bidding of AP funds')
   let accounts = await ethers.getSigners()
-  let tx3 = await usdc
+  let tx3 = await swan
     .connect(accounts[1])
     .approve(bidding.target, ethers.MaxInt256)
   console.log(tx3.hash)
